21d0fbc0f31aadb88938bdbbd11b7c044e3e1772
abhishekmardiya <mardiyaabhishek@gmail.com>
2024-03-25 21:10:44 +0530

    assets , fonts and metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,42 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ReactNode } from "react";
+import { Metadata } from "next";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Nerd Overflow",
+  description:
+    "Discover answers to your coding questions and connect with a vibrant community of developers on our platform. Get expert advice, share knowledge, and level up your programming skills with our comprehensive Q&A platform. Join the conversation today!",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter?.variable} ${spaceGrotesk?.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
